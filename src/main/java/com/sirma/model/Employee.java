31ee39d7e4afc14a8cb37eee2ebfeefb0405d468
Stoycho Stankov <stoycho.stankov@ncb.global>
2022-06-29 09:15:41 +0300

    clean code

--- src/main/java/com/sirma/model/Employee.java
+++ src/main/java/com/sirma/model/Employee.java
@@ -3,7 +3,6 @@ package com.sirma.model;
 import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
